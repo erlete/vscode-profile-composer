de414e86975d82c5338e5845f801708bd5fa896b
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2025-10-26 00:06:26 +0200

    fix: tweak configuration to solve server issues

--- site/next.config.js
+++ site/next.config.js
@@ -3,8 +3,8 @@ const repo = "vscode-profile-composer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? `/${repo}/` : "",
-  basePath: isProd ? `/${repo}` : "",
+  // assetPrefix: isProd ? `/${repo}/` : "",
+  // basePath: isProd ? `/${repo}` : "",
   images: { unoptimized: true },
   output: "export",
   poweredByHeader: false,
