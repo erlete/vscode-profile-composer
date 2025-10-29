993f4b4e06f607b479b9f5c1b2b081970fad37eb
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2025-10-29 02:30:02 +0100

    fix: update dependencies and configuration

---
+++ site/next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+export default {
+  reactStrictMode: true,
+  output: "standalone",
+  poweredByHeader: false,
+  compress: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: false },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = { fs: false, path: false, os: false };
+    }
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.erlete.dev",
+        pathname: "/**",
+      },
+    ],
+  },
+};
