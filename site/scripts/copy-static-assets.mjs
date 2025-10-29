1886f0a4281a4b9283e187ae568ffec55342731e
Paulo Sanchez <dev.szblzpaulo@gmail.com>
2025-10-29 20:17:17 +0100

    fix: solve path issue

--- site/scripts/copy-static-assets.mjs
+++ site/scripts/copy-static-assets.mjs
@@ -5,7 +5,7 @@ import { dirname } from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-const siteRoot = join(__dirname, '..', 'site')
+const siteRoot = join(__dirname, '..')
 
 async function copyStaticAssets() {
   try {
